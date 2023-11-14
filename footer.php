a6e9613d3521102b522882949b4f8fdb30f0faab
thai7777 <149456597+thai7777@users.noreply.github.com>
2023-11-14 09:29:54 +0700

    up

---
+++ footer.php
@@ -0,0 +1,19 @@
+<div class="jumbotron">
+     
+<div class="appeal">            
+        </div>
+ 
+
+<h2>Copyright @ 2023 Design by Dinh Van Duy</h2>
+ 
+</div>
+ 
+</div>
+ 
+<!--/. Container-->
+</div>
+ 
+</div>
+ 
+</body>
+</html>
